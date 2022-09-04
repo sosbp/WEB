2dc7b8744952aa39148edbaeffc160792a298738
Subin Park <so.sbpark@gmail.com>
2022-09-04 21:27:14 +0900

    create CRUD + view template using Mustache

--- src/main/java/com/sbpark/project/springboot/service/posts/PostsService.java
+++ src/main/java/com/sbpark/project/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.sbpark.project.springboot.service.posts;
 
 import com.sbpark.project.springboot.domain.posts.Posts;
 import com.sbpark.project.springboot.domain.posts.PostsRepository;
+import com.sbpark.project.springboot.web.dto.PostsListResponseDto;
 import com.sbpark.project.springboot.web.dto.PostsResponseDto;
 import com.sbpark.project.springboot.web.dto.PostsSaveRequestDto;
 import com.sbpark.project.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,4 +35,18 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true) //readOnly=true -> 조회 속도 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) //.map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
+
 }
---
+++ src/main/java/com/sbpark/project/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.sbpark.project.springboot.web.dto;
+
+import com.sbpark.project.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
